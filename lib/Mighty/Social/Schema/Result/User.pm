a5bf800632241e104fc3bce4a5e44602265e80c3
Adam Taylor <adamjctaylor@gmail.com>
2012-04-25 20:47:44 +0100

    Add User result class

--- lib/Mighty/Social/Schema/Result/User.pm
+++ lib/Mighty/Social/Schema/Result/User.pm
@@ -5,4 +5,30 @@ use warnings;
 
 use parent 'DBIx::Class::Core';
 
+__PACKAGE__->table('user');
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'integer',
+    }.
+    name => {
+        data_type => 'varchar',
+    },
+    email_address => {
+        data_type => 'varchar',
+    },
+    password => {
+        data_type => 'varchar',
+    },
+    account_id => {
+        data_type => 'integer',
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to(
+    'account' => 'Mighty::Social::Schema::Result::Account',
+    { 'foreign.id' => 'self.account_id' },
+);
+
 1;
