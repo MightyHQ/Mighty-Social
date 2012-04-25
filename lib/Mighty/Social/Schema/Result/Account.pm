5e51def4c711cc02c2284c2b4b93e0b2f3e46873
Adam Taylor <adamjctaylor@gmail.com>
2012-04-25 20:44:55 +0100

    Add Account relationship

--- lib/Mighty/Social/Schema/Result/Account.pm
+++ lib/Mighty/Social/Schema/Result/Account.pm
@@ -5,4 +5,38 @@ use warnings;
 
 use parent 'DBIx::Class::Core';
 
+__PACKAGE__->table('account');
+__PACAKGE__->add_columns(
+    id => {
+        data_type => 'integer',
+    }.
+    name => {
+        data_type => 'varchar',
+    },
+    type_id => {
+        data_type => 'integer',
+    },
+    admin_user_id => {
+        data_type => 'integer',
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->has_many(
+    'users' => 'Mighty::Social::Schem::Result::User',
+    { 'foreign.account_id' => 'self.id' }
+);
+
+# XXX many-to-many account-user
+__PACKAGE__->has_many(
+    'websites' => 'Mighty::Social::Schem::Result::User',
+    { 'foreign.account_id' => 'self.id' }
+);
+
+__PACKAGE__->has_one(
+    'admin_user_id' => 'Mighty::Social::Schema::Result::User',
+    { 'foreign.account_id' => 'self.admin_user_id' }
+);
+
 1;
