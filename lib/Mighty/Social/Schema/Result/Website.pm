80767929e1bfef90a1986ddd7394ccd92f3f1058
Adam Taylor <adamjctaylor@gmail.com>
2012-04-25 20:32:33 +0100

    Add Website belongs_to relationship

--- lib/Mighty/Social/Schema/Result/Website.pm
+++ lib/Mighty/Social/Schema/Result/Website.pm
@@ -5,4 +5,27 @@ use warnings;
 
 use parent 'DBIx::Class::Core';
 
+__PACKAGE__->table('website');
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'integer',
+    }.
+    name => {
+        data_type => 'varchar',
+    },
+    url => {
+        data_type => 'varchar',
+    },
+    account_id => {
+        data_type => 'integer',
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to(
+    'account' => 'Mighty::Social::Schema::Result::Account',
+    { 'foreign.id' => 'self.artist_id' },
+);
+
 1;
