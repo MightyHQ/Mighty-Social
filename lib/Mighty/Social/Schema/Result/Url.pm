d02f91b5122da77a1aa068d8b553236beeecde5e
Adam Taylor <adamjctaylor@gmail.com>
2012-04-25 20:51:22 +0100

    Add Url relationship

--- lib/Mighty/Social/Schema/Result/Url.pm
+++ lib/Mighty/Social/Schema/Result/Url.pm
@@ -5,4 +5,24 @@ use warnings;
 
 use parent 'DBIx::Class::Core';
 
+__PACKAGE__->table('url');
+__PACAKGE__->add_columns(
+    id => {
+        data_type => 'integer',
+    }.
+    url => {
+        data_type => 'varchar',
+    },
+    website_id => {
+        data_type => 'integer',
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to(
+    'website' => 'Mighty::Social::Schema::Result::website',
+    { 'foreign.id' => 'self.website_id' },
+);
+
 1;
